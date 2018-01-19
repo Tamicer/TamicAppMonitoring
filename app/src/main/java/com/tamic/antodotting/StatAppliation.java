2e08259ecdb9213a218ffe9dd314df19e2cc614b
liuyongkui <liuyongkui726@pingan.com.cn>
2018-01-19 11:39:24 +0800

    add demo

---
+++ app/src/main/java/com/tamic/antodotting/StatAppliation.java
@@ -0,0 +1,25 @@
+package com.tamic.antodotting;
+import android.app.Application;
+
+import com.tamic.statInterface.statsdk.core.TcStatInterface;
+
+
+/**
+ * Created by LIUYONGKUI726 on 2016-04-13.
+ */
+public class StatAppliation extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // you app id
+        int appId = 21212;
+        // assets
+        String fileName = "my_statconfig.json";
+        String url = "http://www.baidu.com";
+        // init statSdk
+        TcStatInterface.initialize(this, appId, "you app chanel", fileName);
+        TcStatInterface.setUrl(url);
+        TcStatInterface.setUploadPolicy(TcStatInterface.UploadPolicy.UPLOAD_POLICY_DEVELOPMENT, TcStatInterface.UPLOAD_TIME_ONE);
+    }
+}
